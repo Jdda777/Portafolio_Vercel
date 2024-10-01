49cecdf37f5c607bd480172639667473b502136e
Jdda777 <juandavidmendozagaspar@gmail.com>
2024-10-01 11:47:07 -0500

    Descripcion y Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,18 @@ function App() {
     <>
       <h1 className="mainTitle">Juan David Mendoza Gaspar</h1>
       <h1 className="subTitle">Estudiante Universitario</h1>
+      <div className="description-block">
+        <h3 className="descripcion">
+          Soy un estudiante de Ingeniería Multimedia en la Universidad Autonoma
+          de Occidente. Me especializo en Diseño Web. A lo largo de mi
+          formación, he adquirido habilidades en Tecnologías como Unity, Blender
+          y HTML/CSS. Tengo experiencia en proyectos que integran creatividad y
+          tecnología. Estoy apasionado por la creación de experiencias
+          inmersivas, el diseño de interfaces de usuario y la programación de
+          videojuegos, además busco continuamente mejorar mis competencias en
+          varias áreas de la programación.
+        </h3>
+      </div>
       <div className="skills-block">
         <Skills
           color={"#5377A6"}
--- src/components/Card_list.jsx
+++ src/components/Card_list.jsx
@@ -10,13 +10,13 @@ const Card_list = () => {
           linkUrl={"https://github.com/Jdda777/Responsive.git"}
           title="Página Responsive"
           content="Creación de Página Adaptiva usando HTML Y CSS"
-          imageUrl="https://via.placeholder.com/150"
+          imageUrl="https://disenowebakus.net/imagenes/articulos/diseno-web-responsive-adaptable.jpg"
         />
         <Card
           linkUrl={"https://github.com/edwinmgallego/clase-practica-css.git"}
           title="Plataforma de ingreso"
           content="Replica de la plataforma de ingreso de la UAO"
-          imageUrl="https://via.placeholder.com/150"
+          imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX_otoAMenN_1G-MShXGWt0V3IgHr-2tfSeQ&s"
         />
         <Card
           linkUrl={
@@ -24,7 +24,7 @@ const Card_list = () => {
           }
           title="Portafolio en StackBlitz"
           content="Boceto de Portafolio desplegado en StackBlitz"
-          imageUrl="https://via.placeholder.com/150"
+          imageUrl="https://marcago.com/wp-content/uploads/2022/03/portafolio-creativo-768x467-1.jpg"
         />
       </div>
       <div className="card-list">
@@ -32,19 +32,19 @@ const Card_list = () => {
           linkUrl={"https://github.com/Jdda777/Js_Basics.git"}
           title="Número Igual o Mayor"
           content="La aplicación de logica en funcion flecha esta en la rama main"
-          imageUrl="https://via.placeholder.com/150"
+          imageUrl="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgCULpqMSQ9npD9MESnSRCZs-rZY83s7-3IjScXYYFqU4I6ukQ7ngutELZX2ThJnCWPZq57W127lZyOStsShTzDlhr2KWyhmiUDDztK-maWtitWXnYAoMBsqQdbImTGP5VvzjCS3ZDksPM/s1600/Listado+de+operadores+relacionales.png"
         />
         <Card
           linkUrl={"https://github.com/Jdda777/componentes.git"}
           title="Card"
           content="Implementacion de componentes usando el Framework React"
-          imageUrl="https://via.placeholder.com/150"
+          imageUrl="https://blog.wildix.com/wp-content/uploads/2020/06/react-logo.jpg"
         />
         <Card
           linkUrl={"https://github.com/Jdda777/Contador.git"}
           title="Contador"
-          content="IExperimentación con React usando UseState"
-          imageUrl="https://via.placeholder.com/150"
+          content="Experimentación con React usando UseState"
+          imageUrl="https://miro.medium.com/v2/resize:fit:1400/1*MGlrNmHUaT_2Gku11LLfLw.png"
         />
       </div>
     </>
