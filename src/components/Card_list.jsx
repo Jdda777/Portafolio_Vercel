ef07b38f7eba476aefc604ceedac9338c589c671
Jdda777 <juandavidmendozagaspar@gmail.com>
2024-09-30 22:54:54 -0500

    Tarjeta de los numeros y correccion del color de la letra

--- src/components/Card_list.jsx
+++ src/components/Card_list.jsx
@@ -4,26 +4,38 @@ import Card from "./Card";
 
 const Card_list = () => {
   return (
-    <div className="card-list">
-      <Card
-        linkUrl={"https://github.com/Jdda777/Responsive.git"}
-        title="Página Responsive"
-        content="Creación de Página Adaptiva usando HTML Y CSS"
-        imageUrl="https://via.placeholder.com/150"
-      />
-      <Card
-        linkUrl={"https://github.com/edwinmgallego/clase-practica-css.git"}
-        title="Plataforma de ingreso"
-        content="Replica de la plataforma de ingreso de la UAO"
-        imageUrl="https://via.placeholder.com/150"
-      />
-      <Card
-        linkUrl={"https://stackblitz.com/edit/stackblitz-starters-8regsb?file=page2.html"}
-        title="Portafolio en StackBlitz"
-        content="Boceto de Portafolio desplegado en StackBlitz"
-        imageUrl="https://via.placeholder.com/150"
-      />
-    </div>
+    <>
+      <div className="card-list">
+        <Card
+          linkUrl={"https://github.com/Jdda777/Responsive.git"}
+          title="Página Responsive"
+          content="Creación de Página Adaptiva usando HTML Y CSS"
+          imageUrl="https://via.placeholder.com/150"
+        />
+        <Card
+          linkUrl={"https://github.com/edwinmgallego/clase-practica-css.git"}
+          title="Plataforma de ingreso"
+          content="Replica de la plataforma de ingreso de la UAO"
+          imageUrl="https://via.placeholder.com/150"
+        />
+        <Card
+          linkUrl={
+            "https://stackblitz.com/edit/stackblitz-starters-8regsb?file=page2.html"
+          }
+          title="Portafolio en StackBlitz"
+          content="Boceto de Portafolio desplegado en StackBlitz"
+          imageUrl="https://via.placeholder.com/150"
+        />
+      </div>
+      <div className="card-list">
+        <Card
+          linkUrl={"https://github.com/Jdda777/Js_Basics.git"}
+          title="Número Igual o Mayor"
+          content="La aplicación de logica en funcion flecha esta en la rama main"
+          imageUrl="https://via.placeholder.com/150"
+        />
+      </div>
+    </>
   );
 };
 
